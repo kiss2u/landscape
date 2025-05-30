695e0f88d23fed5b0fa7de509b22e60f7fabec6c
ThisSeanZhang <thisseanzhang@gmail.com>
2025-05-30 06:43:59 -0700

    fix: The generated ppp iface name is too long(#70)

--- landscape-webui/src/lib/pppd.ts
+++ landscape-webui/src/lib/pppd.ts
@@ -1,4 +1,5 @@
 const ADAY = 60 * 60 * 24;
+
 export class PPPDServiceConfig {
   attach_iface_name: string;
   iface_name: string;
@@ -11,10 +12,11 @@ export class PPPDServiceConfig {
     enable?: boolean;
     pppd_config?: PPPDConfig;
   }) {
+    let date_str = (new Date().getTime() % ADAY).toString(36);
     this.attach_iface_name = obj.attach_iface_name;
     this.iface_name =
       obj?.iface_name ??
-      `ppp-${obj.attach_iface_name}-${new Date().getTime() % ADAY}`;
+      `ppp-${obj.attach_iface_name}-${date_str}`.substring(0, 15);
     this.enable = obj?.enable ?? true;
     this.pppd_config = new PPPDConfig(obj?.pppd_config);
   }
