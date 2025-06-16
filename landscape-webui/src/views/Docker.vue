45b53c99dac996b9f1d807b4bf331d1a3343018d
ThisSeanZhang <thisseanzhang@gmail.com>
2025-06-16 05:03:02 -0700

    * fix docker page exhibit

--- landscape-webui/src/views/Docker.vue
+++ landscape-webui/src/views/Docker.vue
@@ -3,10 +3,12 @@ import DockerAllContainer from "@/components/docker/DockerAllContainer.vue";
 import DockerStatusCard from "@/components/docker/DockerStatusCard.vue";
 </script>
 <template>
-  <n-flex style="flex: 1; padding-right: 15px" vertical>
-    <n-flex><DockerStatusCard></DockerStatusCard> </n-flex>
-    <n-flex style="flex: 1">
-      <DockerAllContainer></DockerAllContainer>
+  <n-layout :native-scrollbar="false" content-style="padding: 10px;">
+    <n-flex style="flex: 1; padding-right: 15px" vertical>
+      <n-flex><DockerStatusCard></DockerStatusCard> </n-flex>
+      <n-flex style="flex: 1">
+        <DockerAllContainer></DockerAllContainer>
+      </n-flex>
     </n-flex>
-  </n-flex>
+  </n-layout>
 </template>
