c137f964c479cf8b60fa4674f54ed2978d3b704e
ThisSeanZhang <thisseanzhang@gmail.com>
2025-07-17 04:22:33 -0700

    replace text input with dropdown selector for flow config

--- landscape-webui/src/components/flow/FlowDnsMark.vue
+++ landscape-webui/src/components/flow/FlowDnsMark.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { get_flow_rules } from "@/api/flow";
 import { FlowDnsMarkType } from "@/lib/default_value";
 import { FlowDnsMark } from "@/rust_bindings/flow";
+import { computed, onMounted, ref } from "vue";
 
 const mark = defineModel<FlowDnsMark>("mark", { required: true });
 
@@ -21,27 +23,48 @@ const mark_type_option = [
     label: "重定向至流",
     value: FlowDnsMarkType.Redirect,
   },
-  {
-    label: "允许端口共享",
-    value: FlowDnsMarkType.AllowReusePort,
-  },
+  // {
+  //   label: "允许端口共享",
+  //   value: FlowDnsMarkType.AllowReusePort,
+  // },
 ];
+
+onMounted(async () => {
+  await search_flows();
+});
+
+const flow_rules = ref<any[]>([]);
+const flow_options = computed(() => {
+  return flow_rules.value.map((e) => ({
+    value: e.flow_id,
+    label: e.remark ? `${e.flow_id} - ${e.remark}` : e.flow_id,
+  }));
+});
+const flow_search_loading = ref(false);
+async function search_flows() {
+  flow_rules.value = await get_flow_rules();
+}
 </script>
 
 <template>
   <n-input-group>
     <n-select
-      style="width: 38%"
+      style="width: 50%"
       v-model:value="mark.t"
       :options="mark_type_option"
       placeholder="选择匹配方式"
     />
-    <n-input-number
-      :show-button="false"
+    <n-select
+      style="width: 50%"
       v-if="mark.t === FlowDnsMarkType.Redirect"
-      placeholder="重定向的流 ID"
       v-model:value="mark.flow_id"
-      type="text"
+      filterable
+      placeholder="重定向的流 ID"
+      :options="flow_options"
+      :loading="flow_search_loading"
+      clearable
+      remote
+      @search="search_flows"
     />
   </n-input-group>
 </template>
