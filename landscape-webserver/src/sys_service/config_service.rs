bd60dee8b3fedf194651af911f9efcf570db4627
ThisSeanZhang <thisseanzhang@gmail.com>
2025-07-18 08:44:58 -0700

    fix: init config export

--- landscape-webserver/src/sys_service/config_service.rs
+++ landscape-webserver/src/sys_service/config_service.rs
@@ -1,39 +1,16 @@
-use axum::response::IntoResponse;
-use axum::{
-    extract::State,
-    http::{header, HeaderMap, HeaderValue},
-    response::Response,
-    routing::get,
-    Router,
-};
-use chrono::Local;
+use axum::{extract::State, routing::get, Router};
 
+use crate::api::LandscapeApiResp;
+use crate::error::LandscapeApiResult;
 use crate::LandscapeApp;
 
 pub async fn get_config_paths() -> Router<LandscapeApp> {
     Router::new().route("/config/export", get(export_init_config))
 }
 
-async fn export_init_config(State(state): State<LandscapeApp>) -> Response {
+async fn export_init_config(State(state): State<LandscapeApp>) -> LandscapeApiResult<String> {
     let config = state.config_service.export_init_config().await;
     let config_file_content = toml::to_string(&config).unwrap();
 
-    // 获取当前时间
-    let now = Local::now();
-    let time_str = now.format("%Y-%m-%d_%H-%M-%S").to_string();
-
-    // 构造 HTTP 头部
-    let mut headers = HeaderMap::new();
-    headers.insert(header::CONTENT_TYPE, HeaderValue::from_static("application/octet-stream"));
-    headers.insert(
-        header::CONTENT_DISPOSITION,
-        HeaderValue::from_str(&format!(
-            "attachment; filename=\"landscape_init_{}.toml\"",
-            time_str
-        ))
-        .unwrap(),
-    );
-
-    // 返回 Response
-    (headers, config_file_content).into_response()
+    LandscapeApiResp::success(config_file_content)
 }
--- landscape-webui/src/api/sys/config.ts
+++ landscape-webui/src/api/sys/config.ts
@@ -2,25 +2,12 @@ import axiosService from "@/api";
 
 export async function get_init_config(): Promise<void> {
   try {
-    const response = await axiosService.get(`sys_service/config/export`, {
-      responseType: "blob", // 重点！要加这一行，告诉 axios 你期望拿到 blob（二进制）
-    });
+    const response = await axiosService.get(`sys_service/config/export`);
+    const jsonStr = response.data;
 
-    // 拿到 blob 数据
-    const blob = response.data;
+    let filename = "landscape_init.toml";
 
-    // 解析 Content-Disposition 头，提取文件名（可选）
-    const disposition = response.headers["content-disposition"];
-    let filename = "landscape_init.toml"; // 默认文件名
-
-    if (disposition && disposition.includes("filename=")) {
-      const match = disposition.match(/filename="?([^"]+)"?/);
-      if (match && match[1]) {
-        filename = match[1];
-      }
-    }
-
-    // 创建临时 URL
+    const blob = new Blob([jsonStr], { type: "application/octet-stream" });
     const url = window.URL.createObjectURL(blob);
 
     // 创建 a 标签模拟点击
