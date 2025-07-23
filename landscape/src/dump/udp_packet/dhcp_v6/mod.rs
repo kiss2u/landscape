588e5fa02ddd1d71e9954422b3d71871e9a6ed75
ThisSeanZhang <thisseanzhang@gmail.com>
2025-07-23 06:50:45 -0700

    fix CTCC cannot get IPv6 prefix

--- landscape/src/dump/udp_packet/dhcp_v6/mod.rs
+++ landscape/src/dump/udp_packet/dhcp_v6/mod.rs
@@ -3,7 +3,7 @@ use dhcproto::v6::{DhcpOption, DhcpOptions, OptionCode, IAPD, ORO};
 pub fn get_solicit_options() -> DhcpOptions {
     let mut options = DhcpOptions::new();
     let oro = ORO {
-        opts: vec![OptionCode::DomainSearchList, OptionCode::IAPrefix],
+        opts: vec![OptionCode::IAPrefix, OptionCode::DomainNameServers],
     };
 
     let iapd = IAPD { id: 1, t1: 0, t2: 0, opts: DhcpOptions::new() };
