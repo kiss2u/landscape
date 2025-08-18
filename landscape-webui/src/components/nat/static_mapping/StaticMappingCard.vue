9834c3282966e387f7190bfe6d1b1b86fb792cee
ThisSeanZhang <thisseanzhang@gmail.com>
2025-08-18 09:29:24 -0700

    refactor: redesign static NAT mapping config UI card

--- landscape-webui/src/components/nat/static_mapping/StaticMappingCard.vue
+++ landscape-webui/src/components/nat/static_mapping/StaticMappingCard.vue
@@ -19,17 +19,17 @@ async function del() {
   }
 }
 
-const title = computed(() => {
-  const wan_iface_name = rule.value.wan_iface_name
-    ? `${rule.value.wan_iface_name}:`
-    : "any wan";
+// const title = computed(() => {
+//   const wan_iface_name = rule.value.wan_iface_name
+//     ? `${rule.value.wan_iface_name}:`
+//     : "any wan";
 
-  const target_str =
-    rule.value.lan_ip === "0.0.0.0" || rule.value.lan_ip === "::"
-      ? `Route:${rule.value.lan_port}`
-      : `${rule.value.lan_ip}:${rule.value.lan_port}`;
-  return `${wan_iface_name}:${rule.value.wan_port} => ${target_str}`;
-});
+//   const target_str =
+//     rule.value.lan_ip === "0.0.0.0" || rule.value.lan_ip === "::"
+//       ? `Route:${rule.value.lan_port}`
+//       : `${rule.value.lan_ip}:${rule.value.lan_port}`;
+//   return `${wan_iface_name}:${rule.value.wan_port} => ${target_str}`;
+// });
 </script>
 <template>
   <n-flex>
@@ -39,36 +39,88 @@ const title = computed(() => {
           <n-icon :color="rule.enable ? themeVars.successColor : ''" size="14">
             <DotMark />
           </n-icon>
-          {{ title }}
+          <n-ellipsis>
+            {{ rule.remark !== "" ? rule.remark : "无备注" }}
+          </n-ellipsis>
         </n-flex>
       </template>
       <!-- {{ rule }} -->
 
-      <n-descriptions bordered label-placement="top" :column="2">
+      <n-descriptions
+        size="small"
+        bordered
+        label-placement="left"
+        label-align="center"
+        label-style="width: 85px"
+        :column="2"
+      >
         <!-- <n-descriptions-item label="启用">
           <n-tag :bordered="false" :type="rule.enable ? 'success' : ''">
             {{ rule.enable }}
           </n-tag>
         </n-descriptions-item> -->
-        <!-- <n-descriptions-item label="流量标记">
-          {{ rule.mark }}
-        </n-descriptions-item> -->
-        <n-descriptions-item label="备注">
-          {{ rule.remark === "" ? "无备注" : rule.remark }}
+        <n-descriptions-item label="IPv4 映射">
+          {{
+            rule.lan_ipv4
+              ? `${rule.wan_port} => ${rule.lan_ipv4}:${rule.lan_port}`
+              : "无映射"
+          }}
         </n-descriptions-item>
-        <!-- <n-descriptions-item label="匹配规则">
-          {{ rule.items }}
+
+        <n-descriptions-item label="协议" content-style="width:120px">
+          <n-flex>
+            <n-tag
+              :bordered="false"
+              v-for="e in rule.ipv4_l4_protocol"
+              :type="e === 6 ? 'success' : 'info'"
+            >
+              {{ e === 6 ? "TCP" : "UDP" }}
+            </n-tag>
+          </n-flex>
+        </n-descriptions-item>
+
+        <n-descriptions-item label="IPv6 映射">
+          {{
+            rule.lan_ipv6
+              ? `${rule.wan_port} => [${rule.lan_ipv6}]:${rule.lan_port}`
+              : "无映射"
+          }}
+        </n-descriptions-item>
+
+        <n-descriptions-item label="协议" content-style="width:120px">
+          <n-flex>
+            <n-tag
+              :bordered="false"
+              v-for="e in rule.ipv6_l4_protocol"
+              :type="e === 6 ? 'success' : 'info'"
+            >
+              {{ e === 6 ? "TCP" : "UDP" }}
+            </n-tag>
+          </n-flex>
+        </n-descriptions-item>
+        <!-- <n-descriptions-item label="备注">
+          {{ rule.remark === "" ? "无备注" : rule.remark }}
         </n-descriptions-item> -->
+        <n-descriptions-item label="创建时间">
+          <n-time :time="rule.update_at" format="yyyy-MM-dd hh:mm:ss" />
+        </n-descriptions-item>
       </n-descriptions>
       <template #header-extra>
         <n-flex>
-          <n-button type="warning" secondary @click="show_edit_modal = true">
+          <n-button
+            size="small"
+            type="warning"
+            secondary
+            @click="show_edit_modal = true"
+          >
             编辑
           </n-button>
 
           <n-popconfirm @positive-click="del()">
             <template #trigger>
-              <n-button type="error" secondary @click=""> 删除 </n-button>
+              <n-button size="small" type="error" secondary @click="">
+                删除
+              </n-button>
             </template>
             确定删除吗
           </n-popconfirm>
